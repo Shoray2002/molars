7f5a174d28066ecc2ea1d1a168c73619ad9008a3
shoray singhal <shoryasinghall@gmail.com>
2022-06-17 00:26:28 +0530

    ffd complete

--- main.js
+++ main.js
@@ -1,52 +1,57 @@
-var camera, scene, renderer, user_options, orbit_ctrl, trfm_ctrl, orig_geom,smooth_geom;
+let camera,
+  scene,
+  renderer,
+  user_options,
+  orbit_ctrl,
+  trfm_ctrl,
+  orig_geom,
+  smooth_geom;
 const exportButton = document.getElementById("export");
 const canvas = document.getElementById("canvas");
-// Subdivision surface
-var MIN_SUBD_LEVEL = 0;
-var MAX_SUBD_LEVEL = 4;
-var subd_level = 2;
-var smooth_mesh;
-var smooth_verts_undeformed = [];
-var model_index = 1;
-var model_scale;
+let subd_level = 0;
+let smooth_mesh;
+let smooth_verts_undeformed = [];
+let model_scale;
 
 // FFD: control points of a lattice
-var ffd = new FFD();
-var MIN_SPAN_COUNT = 1;
-var MAX_SPAN_COUNT = 8;
-var span_counts = [2, 2, 2];
-var ctrl_pt_geom = new THREE.SphereGeometry(5);
-var ctrl_pt_material = new THREE.MeshLambertMaterial({ color: 0x4d4dff });
-var ctrl_pt_meshes = [];
-var ctrl_pt_mesh_selected = null;
-var lattice_lines = [];
-var lattice_line_material = new THREE.LineBasicMaterial({ color: 0x4d4dff });
+let ffd = new FFD();
+let MIN_SPAN_COUNT = 1;
+let MAX_SPAN_COUNT = 8;
+let span_counts = [2, 2, 2];
+let ctrl_pt_geom = new THREE.SphereGeometry(2.5);
+let ctrl_pt_material = new THREE.MeshLambertMaterial({ color: 0xfff000 });
+let ctrl_pt_meshes = [];
+let ctrl_pt_mesh_selected = null;
+let lattice_lines = [];
+let lattice_line_material = new THREE.LineBasicMaterial({
+  color: 0x000000,
+  transparent: true,
+  opacity: 0.3,
+});
 
 // Evaluated points
-var eval_pt_spans = new THREE.Vector3(16, 16, 16);
-var eval_pt_counts = new THREE.Vector3(
+let eval_pt_spans = new THREE.Vector3(16, 16, 16);
+let eval_pt_counts = new THREE.Vector3(
   eval_pt_spans.x + 1,
   eval_pt_spans.y + 1,
   eval_pt_spans.z + 1
 );
-var eval_pts_geom = new THREE.Geometry();
-var eval_pts_mesh;
-var show_eval_pts_checked = false;
+let eval_pts_geom = new THREE.Geometry();
+let eval_pts_mesh;
+let show_eval_pts_checked = false;
 
-var raycaster = new THREE.Raycaster();
-var mouse = new THREE.Vector2();
+let raycaster = new THREE.Raycaster();
+let mouse = new THREE.Vector2();
 
 const exporter = new THREE.STLExporter();
-var createSomething = function (klass, args) {
-  var F = function (klass, args) {
+let createSomething = function (klass, args) {
+  let F = function (klass, args) {
     return klass.apply(this, args);
   };
   F.prototype = klass.prototype;
   return new F(klass, args);
 };
 
-// Models
-var modelLibrary = [{ type: "BoxGeometry", args: [400, 200, 200, 2, 2, 2] }];
 const loaderSTL = new THREE.STLLoader();
 
 // start scene
@@ -60,12 +65,12 @@ function init() {
     1,
     100000
   );
-  camera.position.z = 500;
+  camera.position.z = 1500;
 
   scene = new THREE.Scene();
 
   // Light
-  var light = new THREE.PointLight(0xffffff, 1.5);
+  let light = new THREE.PointLight(0xffffff, 1.5);
   light.position.set(1000, 1000, 2000);
   scene.add(light);
 
@@ -81,8 +86,6 @@ function init() {
   orbit_ctrl = new THREE.OrbitControls(camera, renderer.domElement);
   orbit_ctrl.damping = 0.2;
   orbit_ctrl.addEventListener("change", render);
-
-  // Transform control (a triad)
   trfm_ctrl = new THREE.TransformControls(camera, renderer.domElement);
   trfm_ctrl.addEventListener("change", render);
   scene.add(trfm_ctrl);
@@ -98,9 +101,9 @@ function init() {
   addModel();
 
   exportButton.addEventListener("click", function () {
-    var stl = exporter.parse(smooth_mesh);
-    var blob = new Blob([stl], { type: "text/plain" });
-    var a = document.createElement("a");
+    let stl = exporter.parse(smooth_mesh);
+    let blob = new Blob([stl], { type: "text/plain" });
+    let a = document.createElement("a");
     a.href = URL.createObjectURL(blob);
     a.download = "smooth_mesh.stl";
     a.click();
@@ -119,10 +122,8 @@ function onDocumentMouseMove(event) {
   mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
   mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
   raycaster.setFromCamera(mouse, camera);
-  var intersects = raycaster.intersectObjects(ctrl_pt_meshes);
-  // If the mouse cursor is hovering over a new control point...
+  let intersects = raycaster.intersectObjects(ctrl_pt_meshes);
   if (intersects.length > 0 && ctrl_pt_mesh_selected != intersects[0].object) {
-    // Temporarily change the cursor shape to a fingering cursor.
     canvas.style.cursor = "pointer";
   } else {
     canvas.style.cursor = "auto";
@@ -134,21 +135,13 @@ function onDocumentMouseDown(event) {
   mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
   mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
   raycaster.setFromCamera(mouse, camera);
-  var intersects = raycaster.intersectObjects(ctrl_pt_meshes);
-  // If a new control point is selected...
+  let intersects = raycaster.intersectObjects(ctrl_pt_meshes);
   if (intersects.length > 0 && ctrl_pt_mesh_selected != intersects[0].object) {
-    // Temporarily disable the orbit control. This prevents the user from
-    // getting surprised or annoyed by the scene rotation as soon as a new
-    // control point is selected.
     orbit_ctrl.enabled = false;
-    // If a control point was selected before, detach it from the transform control.
     if (ctrl_pt_mesh_selected) trfm_ctrl.detach(trfm_ctrl.object);
-    // Remember the new selection to avoid reselecting the same one.
     ctrl_pt_mesh_selected = intersects[0].object;
-    // Attach the newly selected control point to the transform control.
     trfm_ctrl.attach(ctrl_pt_mesh_selected);
   } else {
-    // Enable the orbit control so that the user can pan/rotate/zoom.
     orbit_ctrl.enabled = true;
   }
 }
@@ -164,18 +157,6 @@ function render() {
   renderer.render(scene, camera);
 }
 
-function nextSubdivisionLevel(step) {
-  var old_level = subd_level;
-
-  // Cap the subdivision level to the bounds of [ MIN_SUBD_LEVEL, MAX_SUBD_LEVEL ].
-  subd_level = Math.max(
-    MIN_SUBD_LEVEL,
-    Math.min(subd_level + step, MAX_SUBD_LEVEL)
-  );
-
-  if (subd_level != old_level) addModel();
-}
-
 function addModel() {
   loaderSTL.load("./mesh.stl", function (geometry) {
     orig_geom = geometry;
@@ -183,32 +164,21 @@ function addModel() {
       scene.remove(group);
       scene.remove(smooth_mesh);
     }
-
-    var subd_modifier = new THREE.SubdivisionModifier(subd_level);
-    // var model = modelLibrary[model_index];
-    // orig_geom = createSomething(THREE[model.type], model.args);
-
-    // // Scale geometry.
-    // if (model.scale) orig_geom.scale(model.scale, model.scale, model.scale);
-
-    // Cloning original geometry for debuging
+    let subd_modifier = new THREE.SubdivisionModifier(subd_level);
     smooth_geom = orig_geom.clone();
-
-    // Merge duplicate vertices.
     smooth_geom.mergeVertices();
     smooth_geom.computeFaceNormals();
     smooth_geom.computeVertexNormals();
-
     subd_modifier.modify(smooth_geom);
 
-    var faceABCD = "abcd";
-    var color, f, p, n, vertexIndex;
+    let faceABCD = "abcd";
+    let color, f, p, n, vertexIndex;
 
     for (let i = 0; i < smooth_geom.faces.length; i++) {
       f = smooth_geom.faces[i];
       n = f instanceof THREE.Face3 ? 3 : 4;
 
-      for (var j = 0; j < n; j++) {
+      for (let j = 0; j < n; j++) {
         vertexIndex = f[faceABCD.charAt(j)];
 
         p = smooth_geom.vertices[vertexIndex];
@@ -222,31 +192,16 @@ function addModel() {
 
     const group = new THREE.Group();
     scene.add(group);
-
-    // Mesh for the original model
-    var display_orig_mesh = false;
-    if (display_orig_mesh) {
-      var orig_material = new THREE.MeshBasicMaterial({
-        color: 0xfefefe,
-        wireframe: true,
-        opacity: 0.5,
-      });
-      var orig_mesh = new THREE.Mesh(orig_geom, orig_material);
-      group.add(orig_mesh);
-    }
-
-    // Mesh for the smooth model
-    var smooth_materials = [
+    let smooth_materials = [
       new THREE.MeshPhongMaterial({
         color: 0xffffff,
-        shading: THREE.FlatShading,
-        vertexColors: THREE.VertexColors,
-        shininess: 0,
+        specular: 0xe5e5e5,
+        shininess: 10,
       }),
       new THREE.MeshBasicMaterial({
-        color: 0x405040,
+        color: 0x000000,
         wireframe: true,
-        opacity: 0.8,
+        opacity: 0.1,
         transparent: true,
       }),
     ];
@@ -254,7 +209,6 @@ function addModel() {
       smooth_geom,
       smooth_materials
     );
-
     model_scale = 1;
     smooth_mesh.scale.x = model_scale;
     smooth_mesh.scale.y = model_scale;
@@ -263,15 +217,12 @@ function addModel() {
     scene.add(smooth_mesh);
 
     group.scale.copy(smooth_mesh.scale);
-
-    // Store the vert positions of the smooth model. Empty the storage first.
     smooth_verts_undeformed.length = 0;
     for (let i = 0; i < smooth_geom.vertices.length; i++) {
-      var copy_pt = new THREE.Vector3();
+      let copy_pt = new THREE.Vector3();
       copy_pt.copy(smooth_geom.vertices[i]);
       smooth_verts_undeformed.push(copy_pt);
     }
-
     rebuildFFD(false);
   });
 }
@@ -280,98 +231,86 @@ function rebuildFFD(span_count_change_only) {
   removeCtrlPtMeshes();
   removeLatticeLines();
 
-  var bbox;
+  let bbox;
   if (span_count_change_only) {
     bbox = ffd.getBoundingBox();
   } else {
     bbox = new THREE.Box3();
-    // Compute the bounding box that encloses all vertices of the smooth model.
     bbox.setFromPoints(smooth_geom.vertices);
-    // Scale the bounding box if necessary.
     if (model_scale != 1)
       bbox.set(
         bbox.min.multiplyScalar(model_scale),
         bbox.max.multiplyScalar(model_scale)
       );
   }
-
-  // Do not pass span_counts to ffd.
-  var span_counts_copy = [span_counts[0], span_counts[1], span_counts[2]];
-
-  // Rebuild the lattice with new control points.
+  let span_counts_copy = [span_counts[0], span_counts[1], span_counts[2]];
   ffd.rebuildLattice(bbox, span_counts_copy);
-
   addCtrlPtMeshes();
   addLatticeLines();
-
   deform();
 }
 
 function removeCtrlPtMeshes() {
-  for (var i = 0; i < ctrl_pt_meshes.length; i++)
+  for (let i = 0; i < ctrl_pt_meshes.length; i++)
     scene.remove(ctrl_pt_meshes[i]);
   ctrl_pt_meshes.length = 0;
 }
 
 function removeLatticeLines() {
-  for (var i = 0; i < lattice_lines.length; i++) scene.remove(lattice_lines[i]);
+  for (let i = 0; i < lattice_lines.length; i++) scene.remove(lattice_lines[i]);
   lattice_lines.length = 0;
 }
 
 function addCtrlPtMeshes() {
-  for (var i = 0; i < ffd.getTotalCtrlPtCount(); i++) {
-    var ctrl_pt_mesh = new THREE.Mesh(ctrl_pt_geom, ctrl_pt_material);
+  for (let i = 0; i < ffd.getTotalCtrlPtCount(); i++) {
+    let ctrl_pt_mesh = new THREE.Mesh(ctrl_pt_geom, ctrl_pt_material);
     ctrl_pt_mesh.position.copy(ffd.getPosition(i));
     ctrl_pt_mesh.material.ambient = ctrl_pt_mesh.material.color;
-
     ctrl_pt_meshes.push(ctrl_pt_mesh);
     scene.add(ctrl_pt_mesh);
   }
 }
 
 function addLatticeLines() {
-  // Lines in S direction.
-  for (var i = 0; i < ffd.getCtrlPtCount(0) - 1; i++) {
-    for (var j = 0; j < ffd.getCtrlPtCount(1); j++) {
-      for (var k = 0; k < ffd.getCtrlPtCount(2); k++) {
-        var geometry = new THREE.Geometry();
+  for (let i = 0; i < ffd.getCtrlPtCount(0) - 1; i++) {
+    for (let j = 0; j < ffd.getCtrlPtCount(1); j++) {
+      for (let k = 0; k < ffd.getCtrlPtCount(2); k++) {
+        let geometry = new THREE.Geometry();
         geometry.vertices.push(ctrl_pt_meshes[ffd.getIndex(i, j, k)].position);
         geometry.vertices.push(
           ctrl_pt_meshes[ffd.getIndex(i + 1, j, k)].position
         );
-        var line = new THREE.Line(geometry, lattice_line_material);
+        let line = new THREE.Line(geometry, lattice_line_material);
 
         lattice_lines.push(line);
         scene.add(line);
       }
     }
   }
-  // Lines in T direction.
-  for (var i = 0; i < ffd.getCtrlPtCount(0); i++) {
-    for (var j = 0; j < ffd.getCtrlPtCount(1) - 1; j++) {
-      for (var k = 0; k < ffd.getCtrlPtCount(2); k++) {
-        var geometry = new THREE.Geometry();
+  for (let i = 0; i < ffd.getCtrlPtCount(0); i++) {
+    for (let j = 0; j < ffd.getCtrlPtCount(1) - 1; j++) {
+      for (let k = 0; k < ffd.getCtrlPtCount(2); k++) {
+        let geometry = new THREE.Geometry();
         geometry.vertices.push(ctrl_pt_meshes[ffd.getIndex(i, j, k)].position);
         geometry.vertices.push(
           ctrl_pt_meshes[ffd.getIndex(i, j + 1, k)].position
         );
-        var line = new THREE.Line(geometry, lattice_line_material);
+        let line = new THREE.Line(geometry, lattice_line_material);
 
         lattice_lines.push(line);
         scene.add(line);
       }
     }
   }
-  // Lines in U direction.
-  for (var i = 0; i < ffd.getCtrlPtCount(0); i++) {
-    for (var j = 0; j < ffd.getCtrlPtCount(1); j++) {
-      for (var k = 0; k < ffd.getCtrlPtCount(2) - 1; k++) {
-        var geometry = new THREE.Geometry();
+  for (let i = 0; i < ffd.getCtrlPtCount(0); i++) {
+    for (let j = 0; j < ffd.getCtrlPtCount(1); j++) {
+      for (let k = 0; k < ffd.getCtrlPtCount(2) - 1; k++) {
+        let geometry = new THREE.Geometry();
         geometry.vertices.push(ctrl_pt_meshes[ffd.getIndex(i, j, k)].position);
         geometry.vertices.push(
           ctrl_pt_meshes[ffd.getIndex(i, j, k + 1)].position
         );
-        var line = new THREE.Line(geometry, lattice_line_material);
+        let line = new THREE.Line(geometry, lattice_line_material);
 
         lattice_lines.push(line);
         scene.add(line);
@@ -381,11 +320,10 @@ function addLatticeLines() {
 }
 
 function createEvalPtsMesh() {
-  var total_eval_pts_count =
+  let total_eval_pts_count =
     eval_pt_counts.x * eval_pt_counts.y * eval_pt_counts.z;
-  for (var i = 0; i < total_eval_pts_count; i++)
+  for (let i = 0; i < total_eval_pts_count; i++)
     eval_pts_geom.vertices.push(new THREE.Vector3());
-  // Red dot
   eval_pts_mesh = new THREE.Points(
     eval_pts_geom.clone(),
     new THREE.PointsMaterial({ color: 0xff0000, size: 2 })
@@ -394,17 +332,13 @@ function createEvalPtsMesh() {
 }
 
 function updateLattice() {
-  // Update the positions of all control point in the FFD object.
-  for (var i = 0; i < ffd.getTotalCtrlPtCount(); i++)
+  for (let i = 0; i < ffd.getTotalCtrlPtCount(); i++)
     ffd.setPosition(i, ctrl_pt_meshes[i].position);
-
-  // Update the positions of all lines of the lattice.
-  var line_index = 0;
-  // Lines in S direction.
-  for (var i = 0; i < ffd.getCtrlPtCount(0) - 1; i++) {
-    for (var j = 0; j < ffd.getCtrlPtCount(1); j++) {
-      for (var k = 0; k < ffd.getCtrlPtCount(2); k++) {
-        var line = lattice_lines[line_index++];
+  let line_index = 0;
+  for (let i = 0; i < ffd.getCtrlPtCount(0) - 1; i++) {
+    for (let j = 0; j < ffd.getCtrlPtCount(1); j++) {
+      for (let k = 0; k < ffd.getCtrlPtCount(2); k++) {
+        let line = lattice_lines[line_index++];
         line.geometry.vertices[0] =
           ctrl_pt_meshes[ffd.getIndex(i, j, k)].position;
         line.geometry.vertices[1] =
@@ -413,11 +347,10 @@ function updateLattice() {
       }
     }
   }
-  // Lines in T direction.
-  for (var i = 0; i < ffd.getCtrlPtCount(0); i++) {
-    for (var j = 0; j < ffd.getCtrlPtCount(1) - 1; j++) {
-      for (var k = 0; k < ffd.getCtrlPtCount(2); k++) {
-        var line = lattice_lines[line_index++];
+  for (let i = 0; i < ffd.getCtrlPtCount(0); i++) {
+    for (let j = 0; j < ffd.getCtrlPtCount(1) - 1; j++) {
+      for (let k = 0; k < ffd.getCtrlPtCount(2); k++) {
+        let line = lattice_lines[line_index++];
         line.geometry.vertices[0] =
           ctrl_pt_meshes[ffd.getIndex(i, j, k)].position;
         line.geometry.vertices[1] =
@@ -426,11 +359,10 @@ function updateLattice() {
       }
     }
   }
-  // Lines in U direction.
-  for (var i = 0; i < ffd.getCtrlPtCount(0); i++) {
-    for (var j = 0; j < ffd.getCtrlPtCount(1); j++) {
-      for (var k = 0; k < ffd.getCtrlPtCount(2) - 1; k++) {
-        var line = lattice_lines[line_index++];
+  for (let i = 0; i < ffd.getCtrlPtCount(0); i++) {
+    for (let j = 0; j < ffd.getCtrlPtCount(1); j++) {
+      for (let k = 0; k < ffd.getCtrlPtCount(2) - 1; k++) {
+        let line = lattice_lines[line_index++];
         line.geometry.vertices[0] =
           ctrl_pt_meshes[ffd.getIndex(i, j, k)].position;
         line.geometry.vertices[1] =
@@ -442,9 +374,8 @@ function updateLattice() {
 }
 
 function deform() {
-  // Update the model vertices.
   for (let i = 0; i < smooth_geom.vertices.length; i++) {
-    var eval_pt = ffd.evalWorld(smooth_verts_undeformed[i]);
+    let eval_pt = ffd.evalWorld(smooth_verts_undeformed[i]);
     if (eval_pt.equals(smooth_geom.vertices[i])) continue;
     smooth_geom.vertices[i].copy(eval_pt);
   }
